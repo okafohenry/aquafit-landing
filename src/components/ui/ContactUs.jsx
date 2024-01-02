2adbd3be9432a90f83063fab294486b4d802a61f
okafohenry <okafohenrie@gmail.com>
2024-01-02 13:36:27 +0100

    initial commit

--- src/components/ui/ContactUs.jsx
+++ src/components/ui/ContactUs.jsx
@@ -1,7 +1,7 @@
 function ContactUs() {
     return (
         <section className="section-3">
-            <h2>An enterprise template to ramp up your company website</h2>
+            <h2>Fill out the form and we'll send you a message to schedule a trial class for free</h2>
             <form action="#" method="#" className="w-full form gap-x-3 justify-center">
                 <input 
                     type="text" 
--- src/components/ui/ReviewCard.jsx
+++ src/components/ui/ReviewCard.jsx
@@ -9,11 +9,11 @@ function ReviewCard({ userImg, name, reviewText, rating }) {
                 <img src={quote} alt="quote" />
             </div>
             <h4 className="name text-[23px] font-semibold leading-[36px] tracking-wide text-white mb-5">{name}</h4>
-            <p className="review-text text-[#ACB5BB] text-[16px] font-extralight tracking-wider mb-6">{reviewText}</p>
-            <div className="rating w-fit py-1.5 px-4 flex rounded-[16px] items-center gap-x-1 bg-[#1E2836]/[0.6] text-white">
+            <p className="review-text">{reviewText}</p>
+            {/* <div className="rating">
                 <img src={ratingStar} alt="star" />
                 <p>{rating}</p>
-            </div>
+            </div> */}
         </div>
     )
 }
